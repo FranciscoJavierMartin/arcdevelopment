52da4ca66b4e7c303fdd14bc49f67db762bbbd51
Francisco <javiermartincuenca@gmail.com>
2020-08-16 23:16:53 +0200

    Custom software block finished

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import { Grid, Button, Typography } from '@material-ui/core';
+import { Grid, Button, Typography, useMediaQuery } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import ButtonArrow from './ui/ButtonArrow';
+
 import animationData from '../animations/landinganimation/data';
+import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -11,9 +13,9 @@ const useStyles = makeStyles((theme) => ({
     minWidth: '21em',
     marginTop: '2em',
     marginLeft: '10%',
-    [theme.breakpoints.down('sm')]:{
+    [theme.breakpoints.down('sm')]: {
       maxWidth: 30,
-    }
+    },
   },
   estimateButton: {
     ...(theme.typography as any).estimate,
@@ -30,16 +32,19 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '1em',
   },
   learnButtonHero: {
-    borderColor: (theme.palette.common as any).blue,
-    color: (theme.palette.common as any).blue,
-    borderWidth: 2,
-    textTransform: 'none',
-    borderRadius: 50,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
+    ...(theme.typography as any).learnButton,
     fontSize: '0.9rem',
     height: 45,
     width: 145,
+  },
+  learnButton: {
+    ...(theme.typography as any).learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em',
+    },
   },
   mainContainer: {
     marginTop: '5em',
@@ -54,14 +59,34 @@ const useStyles = makeStyles((theme) => ({
     minWidth: '21.5em',
     marginLeft: '1em',
     [theme.breakpoints.down('xs')]: {
-      marginLeft: 0
-    }
+      marginLeft: 0,
+    },
+  },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: (theme.palette.common as any).orange,
+  },
+  subtitle: {
+    marginBottom: '1em',
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25,
+    },
   },
 }));
 
 const LandingPage: React.FC = () => {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
 
   const defaultOptions = {
     loop: true,
@@ -74,6 +99,7 @@ const LandingPage: React.FC = () => {
 
   return (
     <Grid container direction='column' className={classes.mainContainer}>
+      {/*------ Hero block ------*/}
       <Grid item>
         <Grid container direction='row' justify='flex-end' alignItems='center'>
           <Grid sm item className={classes.heroTextContainer}>
@@ -109,6 +135,49 @@ const LandingPage: React.FC = () => {
           </Grid>
         </Grid>
       </Grid>
+      {/*------ End Hero block ------*/}
+      <Grid item>
+        {/*------ Services block ------*/}
+        <Grid
+          container
+          direction='row'
+          className={classes.serviceContainer}
+          justify={matchesSM ? 'center' : undefined}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>Custom Software Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Save Energy. Save Time. Save Money.
+            </Typography>
+            <Typography variant='subtitle1'>
+              Complete digital solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration</span>
+            </Typography>
+            <Button variant='outlined' className={classes.learnButton}>
+              <span style={{ marginRight: 15 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={(theme.palette.common as any).blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              alt='custom software icon'
+              src={customSoftwareIcon}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      {/*------ End Services block ------*/}
     </Grid>
   );
 };
--- src/components/ui/Theme.tsx
+++ src/components/ui/Theme.tsx
@@ -2,6 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const arcBlue = '#0B72B9';
 const arcOrange = '#FFBA60';
+const arcGrey = '#868686';
 
 export default createMuiTheme(undefined, {
   palette: {
@@ -34,8 +35,24 @@ export default createMuiTheme(undefined, {
       fontFamily: 'Raleway',
       fontWeight: 700,
       fontSize: '2.5rem',
-      color: `${arcBlue}`,
+      color: arcBlue,
       lineHeight: 1.5,
+    },
+    h3: {
+      fontFamily: 'Pacifico',
+      fontSize: '2.5rem',
+      color: arcBlue,
+    },
+    h4: {
+      fontFamily: 'Raleway',
+      fontSize: '1.75rem',
+      color: arcBlue,
+      fontWeight: 700,
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      color: arcGrey,
     },
     learnButton: {
       borderColor: arcBlue,
